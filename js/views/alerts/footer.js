4f0e0a65f54f5e4c3d7ade4582b5234f03024f40
Andrew Hart <drew.pressedweb@gmail.com>
2014-04-12 18:46:01 -0400

    Cleaning up

--- js/views/alerts/footer.js
+++
@@ -1,6 +0,0 @@
-Application.View.extend({
-  name: "alerts/footer"
-});
-
-// Instances of this view can be created by calling:
-// new Application.Views["alerts/footer"]()
--- js/views/alerts/header.js
+++
@@ -1,6 +0,0 @@
-Application.View.extend({
-  name: "alerts/header"
-});
-
-// Instances of this view can be created by calling:
-// new Application.Views["alerts/header"]()
