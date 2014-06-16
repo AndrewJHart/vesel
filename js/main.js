bc9dc4f8371b378e177f556e3e9f977e514e2ed5
jonhairston <jonhairston@outlook.com>
2014-06-16 16:38:00 -0400

    -- was attempting to make the router differentiate between two different sizes for the ipad and iphone    views but now we're going to make the ipad and iphone views two separate pages. Leaving in for now.

--- js/main.js
+++ js/main.js
@@ -30,175 +30,18 @@ require([
             silent: true
         });
 
-        // first thing -- set this to first run!! 
-        firstRun = store.get('firstRun');
-        if (!firstRun) {
-            // show the slide view first by pointing backbone to 
-            // different route and ensure we only POST once
-            createUserDeviceAccount(store.get('registration_id'));
+        
+        // RootView may use link or url helpers which
+        // depend on Backbone history being setup
+        // so need to wait to loadUrl() (which will)
+        // actually execute the route
+        RootView.getInstance(document.body);
 
-            // RootView may use link or url helpers which
-            // depend on Backbone history being setup
-            // so need to wait to loadUrl() (which will)
-            // actually execute the route
-            RootView.getInstance(document.body);
+        // Instantiate the main router
+        new Router();
 
-            // Instantiate the main router
-            new Router();
-
-            // This will trigger your routers to start
-            Backbone.history.loadUrl('#intro');
-
-            store.set('firstRun', true);
-        } else {
-            // RootView may use link or url helpers which
-            // depend on Backbone history being setup
-            // so need to wait to loadUrl() (which will)
-            // actually execute the route
-            RootView.getInstance(document.body);
-
-            // Instantiate the main router
-            new Router();
-
-            // This will trigger your routers to start
-            Backbone.history.loadUrl();
-
-        }
+        // This will trigger your routers to start
+        Backbone.history.loadUrl('alerts');
 
     })();
-
-    // delegate to wrap ajax calls for registering with our server
-    function createUserDeviceAccount(token) {
-        store.set('username', "Anon" + Date.now() + Math.floor(Math.random() * (5000 - 500) + 500));
-        store.set('region', 1);
-
-        // we now have a new registration id & need to save it to the server along w/ its related categories
-        $.ajax({
-            url: 'https://heads-up.herokuapp.com/api/app/v2/device_settings/ios/',
-            type: 'POST',
-            data: JSON.stringify({
-                "device": {
-                    "token": token,
-                    "user": {
-                        "username": store.get('username'),
-                        "password": Date.now() + Math.floor(Math.random() * (1000 - 1) + 1),
-                        "region_set": {
-                            "name": store.get('region')
-                        }
-                    }
-                },
-                "global_priority": 1
-            }),
-            contentType: 'application/json',
-            success: function(data, status) {
-                console.log('POSTed to reg_id to server!');
-                console.log('Data resp is:');
-                console.log(data.device.user.api_key.key);
-
-                store.set('api_key', data.device.user.api_key.key);
-            },
-            error: function(xhr, type) {
-                console.log('** ERROR ON POST **');
-                console.dir(xhr);
-                console.dir(type);
-            }
-        });
-    }
-
-
-    // loads local settings & checks if first run etc...
-    function getLocalSettings() {
-        // var firstRun = store.get('firstRun');
-        firstRun = store.get('firstRun');
-        console.debug('****FirstRun is equal to: ' + firstRun);
-
-        if (firstRun) {
-            window.gAppFirstRun = firstRun;
-        } else {
-            window.gAppFirstRun = true;
-            store.set('firstRun', 'false');
-        }
-    }
-
-    // method to get to root assets path on android or iOS
-    getCordovaFilePath = function() {
-        var path = window.location.pathname;
-        path = path.substr(path, path.length - 10); // -10 for index.html
-        return 'file://' + path;
-    };
-
-    // clear badge and push notification center data
-    clearBadgeData = function() {
-        // if handle to push notifications is good then relieve the notifications center of its data
-        window.plugins.pushNotification.setApplicationIconBadgeNumber(0, function(status) {
-            console.log('Reset the badge icons');
-            console.log(status);
-        });
-        window.plugins.pushNotification.cancelAllLocalNotifications(function() {
-            console.log('Cancelling and clearing all stored apple notifications');
-        });
-    };
-
-    // Cordova Function Hooks -- observables
-    resumeApp = function() {
-        // re-sync with the server -- todo: update to only do this when opened by push notification
-        if (app) {
-            console.log('Resuming.....');
-            Application["alerts"].fetch({
-                wait: true
-            });
-        }
-
-        clearBadgeData();
-    };
-
-    // triggered by cordova when the device is ready
-    onDeviceReady = function() {
-        console.log('device ready');
-
-        // try to get cached copy of the device UUID for model 
-        // just in case nothing has changed on following check to prevent
-        // delay with polling in device model for settings view
-        cached_token = store.get('registration_id');
-
-        if (cached_token) {
-            window.registration_id = cached_token;
-        }
-
-        // register this device with apple
-        window.plugins.pushNotification.register(function(status) {
-            // log the token
-            console.log(status);
-
-            // store on global object
-            if (window.registration_id == status) {
-                console.log('Registration from localstore matches token - no action');
-
-                return;
-            } else {
-                window.registration_id = status;
-
-                // save it to localstorage
-                store.set('registration_id', status);
-
-            }
-
-        }, function(error) {
-            console.log('Error handler called with message');
-            console.log(error);
-        }, {
-            alert: true,
-            badge: true,
-            sound: true
-        });
-
-        // start the app 
-        startApp();
-
-        console.log('******* END OF DEVICE READY *******');
-    };
-
-    // bind listeners for cordova
-    document.addEventListener('deviceready', onDeviceReady, false);
-    document.addEventListener('resume', resumeApp, false);
 });
--- js/routers/routes.js
+++ js/routers/routes.js
@@ -12,12 +12,15 @@ define([
 ], function(Backbone, RootView, HomeView, HeaderView, FooterView, MapView, DetailView, ProfileView, SlidesView, AlertsCollection) {
     return Backbone.Router.extend({
         routes: {
-            "": "index",
+            "": "Iphone",            // this is what will be loaded when the landing page is loaded 
+            "alerts": "Ipad",      // this is what will be loaded when a user goes to the /alerts   # had to chop off the s becuase of the collection name
             "intro": "intro",
             "map": "maplist",
             "about": "about",
             "profile": "profile",
-            "detail/:id": "detail"
+            "detailIphone/:id": "detailIphone",
+            "detailIpad/:id": "detailIpad"
+
         },
 
         // ----------
@@ -39,8 +42,41 @@ define([
         },
 
         // default route, triggered on / or /#
-        index: function(params) {
+        // in this case it will represent the app from inside the iphone on the main page
+        Iphone: function(params) {
             console.log('Index route triggered');
+
+            // grab the url 
+            console.log("this is the index function url: " + this.routes[Backbone.history.fragment]);
+            // only instantiate the alerts collection once
+            if (!this.alerts) {
+                this.alerts = Application["alerts"] = new AlertsCollection();
+            }
+
+            // only instantiate on the initial run
+            if (!this.indexView) {
+                // create an instance of the home page-view (AnimView)
+                this.indexView =
+                    new HomeView({
+                        el: '#home',
+                        className: 'home page',
+                        collection: this.alerts
+                    });
+
+            }
+
+            // Tell the root view to render the view and render it as a page w/ animations
+            Application.goto(this.indexView, {
+                page: true
+            }); // alternative RootView.getInstance().setView(view);
+
+            return this;
+        },
+
+        // default route, triggered on /alerts or /#alerts
+        // in this case will render the app on the scrapable page 
+        Ipad: function(params) {
+            console.log('alerts route triggered');
 
             // only instantiate the alerts collection once
             if (!this.alerts) {
@@ -66,6 +102,7 @@ define([
             return this;
         },
 
+
         intro: function() {
             console.log('Intro route Triggered! ----------');
 
@@ -80,14 +117,14 @@ define([
             return this;
         },
 
-        detail: function(params) {
+        detailIphone: function(params) {
 
             // hoisting 
             var model = null,
                 pageView = null;
 
             // log the route params passed to us
-            console.log("detail/index route received :id " + params);
+            console.log("detailIphone/index route received :id " + params);
 
             // use params to get model from our collection
             model = Application["alerts"].get(params);
@@ -111,6 +148,37 @@ define([
             return this;
         },
 
+
+        detailIpad: function(params) {
+
+            // hoisting 
+            var model = null,
+                pageView = null;
+
+            // log the route params passed to us
+            console.log("detailIpad/index route received :id " + params);
+
+            // use params to get model from our collection
+            model = Application["alerts"].get(params);
+
+            // create the detail page-view that contains the header view,
+            // the footer view, and the actual content view nested within
+            // using the handlebars "view" helper
+            pageView = new DetailView({
+                className: 'detail right',
+                model: model
+            });
+
+            // animate to this view and remove the classname 'right'
+            // after the view is appended to the DOM but right before 
+            // the transition happens. 
+            Application.goto(pageView, {
+                page: true, // this is its own page/pane so tell app to animate it
+                toggleIn: 'right' // remove the class right before animating
+            });
+
+            return this;
+        },
         // triggered when route matches /#map
         maplist: function(params) {
 
